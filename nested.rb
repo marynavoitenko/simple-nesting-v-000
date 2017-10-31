470bb8b076c4416fe76a82047938af36d67c3745
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-31 01:59:20 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,6 +60,11 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+	programmer_hash[:yukihiro_matsumoto] => {
+		:known_for => "Ruby", 
+		:languages => ["LISP", "C"]
+	}
+end
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
